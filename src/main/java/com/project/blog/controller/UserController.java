4bdc85194a813e8964f18c5638ac995bea8c338d
SungGil-5125 <sunggil0125@naver.com>
2022-04-17 17:23:15 +0900

    회원가입 로직

--- src/main/java/com/project/blog/controller/UserController.java
+++ src/main/java/com/project/blog/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.project.blog.controller;
 
+import com.project.blog.dto.User.UserLoginDto;
+import com.project.blog.dto.User.UserSignupDto;
+import com.project.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("blog")
 public class UserController {
 
+    private final UserService userService;
 
+    public UserSignupDto Signup(UserSignupDto userSignupDto) {
+        userService.join(userSignupDto);
+        return userSignupDto;
+    }
+
+    public UserSignupDto Login(UserLoginDto userLoginDto) {
+
+    }
 }
