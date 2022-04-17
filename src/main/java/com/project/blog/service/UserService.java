5900e0dd14e3380313ef0bfe69e51153927a01cf
SungGil-5125 <sunggil0125@naver.com>
2022-04-17 17:23:29 +0900

    Add Login

--- src/main/java/com/project/blog/service/UserService.java
+++ src/main/java/com/project/blog/service/UserService.java
@@ -1,11 +1,13 @@
 package com.project.blog.service;
 
 import com.project.blog.domain.User;
+import com.project.blog.dto.User.UserLoginDto;
 import com.project.blog.dto.User.UserSignupDto;
 import com.project.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User join(UserSignupDto userSignupDto) {
         Optional<User> byEmail = userRepository.findByEmail(userSignupDto.getEmail());
@@ -26,5 +29,12 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public String login(UserLoginDto userLoginDto) {
+        User user = userRepository.findByEmail(userLoginDto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 email입니다."));
 
+        if(!passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword())) {
+                throw new IllegalAccessException("잘못된 비밀번호 입니다.");
+        }
+    }
 }
