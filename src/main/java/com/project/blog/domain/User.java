07d720ac9e5336ba10b18e9b86733d6bdb50849d
SungGil-5125 <sunggil0125@naver.com>
2022-04-18 17:39:43 +0900

    Add ID

--- src/main/java/com/project/blog/domain/User.java
+++ src/main/java/com/project/blog/domain/User.java
@@ -2,18 +2,21 @@ package com.project.blog.domain;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Builder
+@Table(name = "user")
 @NoArgsConstructor // 기본 생성자를 해주는거거든
 @AllArgsConstructor
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
     @Column(name = "email")
     private String email;
 
