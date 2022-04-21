b3f026a4716667e2ac5f900dc7ede5f9cf2ac1b7
SungGil-5125 <sunggil0125@naver.com>
2022-04-21 20:44:56 +0900

    Add GetMapping

--- src/main/java/com/project/blog/controller/UserController.java
+++ src/main/java/com/project/blog/controller/UserController.java
@@ -3,7 +3,7 @@ package com.project.blog.controller;
 import com.project.blog.domain.User;
 import com.project.blog.dto.Request.UserLoginDto;
 import com.project.blog.dto.Request.UserSignupDto;
-import com.project.blog.dto.Response.TokenDto;
+import com.project.blog.dto.Response.TokenResponseDto;
 import com.project.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +15,23 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("user/join")
+    @GetMapping("register")
+    public String register() {
+        return "회원가입 페이지";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        return "로그인 페이지";
+    }
+
+    @PostMapping("register")
     public User Signup(@RequestBody UserSignupDto userSignupDto) {
         return userService.join(userSignupDto);
     }
 
-    @PostMapping("user/login")
-    public TokenDto Login(@RequestBody UserLoginDto userLoginDto) {
+    @PostMapping("login")
+    public TokenResponseDto Login(@RequestBody UserLoginDto userLoginDto) {
         return userService.login(userLoginDto);
     }
 }
