de67ba4e706ab8d3f195001243fd6789670f3221
SungGil-5125 <sunggil0125@naver.com>
2022-04-21 14:24:10 +0900

    Create TestController

--- src/main/java/com/project/blog/controller/TestController.java
+++ src/main/java/com/project/blog/controller/TestController.java
@@ -3,8 +3,6 @@ package com.project.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.Column;
-
 @Controller
 public class TestController {
     @GetMapping("/hello")
