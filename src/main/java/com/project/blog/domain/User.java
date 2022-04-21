22fdb00f1cdea8f89dd898a37d3ed07a43c95e18
SungGil-5125 <sunggil0125@naver.com>
2022-04-21 14:23:16 +0900

    Add UserDetails

--- src/main/java/com/project/blog/domain/User.java
+++ src/main/java/com/project/blog/domain/User.java
@@ -2,9 +2,12 @@ package com.project.blog.domain;
 
 import com.project.blog.enums.Role;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -13,23 +16,53 @@ import java.util.List;
 @NoArgsConstructor // 기본 생성자를 해주는거거든
 @AllArgsConstructor
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Long id;
+    @Column(name = "user_id")
+    private Long user_id;
 
-    @Column(name = "email")
+    @Column
     private String email;
 
-    @Column(name = "password")
+    @Column
     private String password;
 
-    @Column(name = "refreshToken")
-    private String refreshToken;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
-    @ElementCollection(fetch = FetchType.LAZY)
-    @Enumerated(EnumType.STRING)
-    private List<Role> roles = new ArrayList<Role>();
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+//    @Column(name = "refreshToken")
+//    private String refreshToken;
+//
+//    @ElementCollection(fetch = FetchType.LAZY)
+//    @Enumerated(EnumType.STRING)
+//    private List<Role> roles = new ArrayList<Role>();
 }
