4c52317386653d9ed68cd5d17488bcb874b1e385
SungGil-5125 <sunggil0125@naver.com>
2022-04-22 16:08:42 +0900

    Add Cors

---
+++ src/main/java/com/project/blog/config/webConfig/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.project.blog.config.webConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
+                .maxAge(3000);
+    }
+}
