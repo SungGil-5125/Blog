9fb23eafc07dd03a87ad4427e39c663bb7db5bf7
SungGil-5125 <sunggil0125@naver.com>
2022-04-22 16:12:01 +0900

    Delete RequestMapping "blog"

--- src/main/java/com/project/blog/controller/UserController.java
+++ src/main/java/com/project/blog/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("blog")
 public class UserController {
 
     private final UserService userService;
