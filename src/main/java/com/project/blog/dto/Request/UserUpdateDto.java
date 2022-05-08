0a3a3ee231547ccf2065ce892b46c576d5a2e40f
SungGil-5125 <sunggil0125@naver.com>
2022-05-08 11:54:34 +0900

    ✨ toEntity 추가

--- src/main/java/com/project/blog/dto/Request/UserUpdateDto.java
+++ src/main/java/com/project/blog/dto/Request/UserUpdateDto.java
@@ -1,11 +1,10 @@
 package com.project.blog.dto.Request;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.project.blog.domain.User;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +14,15 @@ public class UserUpdateDto {
     private String email;
     private String password;
     private String img;
+
+    public User toEntity(){
+        return User.builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .img(img)
+                .build();
+    }
+
 
 }
