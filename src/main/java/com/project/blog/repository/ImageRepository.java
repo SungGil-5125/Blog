1ae970ebdbf2fd0f99ab641342b1ace8203f54ef
SungGil-5125 <sunggil0125@naver.com>
2022-05-11 20:36:17 +0900

    ✨ :: Add image Repository

---
+++ src/main/java/com/project/blog/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.project.blog.repository;
+
+import com.project.blog.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
