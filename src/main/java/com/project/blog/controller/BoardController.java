dbb89393b1a52adf9dc039ab6731c6d862ebcf90
SungGil-5125 <sunggil0125@naver.com>
2022-05-20 09:19:18 +0900

    :sparkles: :: blog 사진 업로드

--- src/main/java/com/project/blog/controller/BoardController.java
+++ src/main/java/com/project/blog/controller/BoardController.java
@@ -1,15 +1,16 @@
 package com.project.blog.controller;
 
-import com.project.blog.domain.Image;
+import com.project.blog.domain.Board;
+import com.project.blog.dto.Request.BoardCreateRequestDto;
 import com.project.blog.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
@@ -17,12 +18,12 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("board/write")
-    public List<Image> CreateBoard(
-            @RequestPart(value = "file", required = false) List<MultipartFile> file
-            //@RequestPart BoardCreateRequestDto boardCreateRequestDto
+    public Board CreateBoard(
+            @Valid @RequestParam List<MultipartFile> files,
+            @Valid @RequestPart BoardCreateRequestDto boardCreateRequestDto
             ) throws Exception {
 
-        return boardService.CreateBoard(file); //boardCreateRequestDto,
+        return boardService.CreateBoard(files, boardCreateRequestDto); //boardCreateRequestDto,
     }
 
 }
