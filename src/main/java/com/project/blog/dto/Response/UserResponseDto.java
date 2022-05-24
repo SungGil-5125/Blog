bcc71c0d52201e2a9257f5bdc3fb8af6483b9a80
SungGil-5125 <sunggil0125@naver.com>
2022-05-24 21:01:09 +0900

    ✨ :: userResponse 추가

--- src/main/java/com/project/blog/dto/Response/UserResponseDto.java
+++ src/main/java/com/project/blog/dto/Response/UserResponseDto.java
@@ -1,14 +1,20 @@
 package com.project.blog.dto.Response;
 
+import com.project.blog.domain.Board;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.core.io.FileSystemResource;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class UserResponseDto {
 
-    private Long User_id;
-    private String User_email;
-    private String User_name;
+    private Long user_id;
+    private String email;
+    private String name;
+    private String prifile_image;
+    private List<Board> boards;
 
 }
