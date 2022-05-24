bff0323199f3bd30f46625e920f5f2df745ff433
SungGil-5125 <sunggil0125@naver.com>
2022-05-24 21:00:05 +0900

    ✨ :: 이미지 url, 내용 url 분리

--- src/main/java/com/project/blog/controller/UserController.java
+++ src/main/java/com/project/blog/controller/UserController.java
@@ -3,18 +3,16 @@ package com.project.blog.controller;
 import com.project.blog.domain.User;
 import com.project.blog.dto.Request.UserLoginDto;
 import com.project.blog.dto.Request.UserSignupDto;
-import com.project.blog.dto.Request.UserUpdateDto;
 import com.project.blog.dto.Response.TokenResponseDto;
-import com.project.blog.dto.Response.UserResponseDto;
 import com.project.blog.service.UserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.lang.management.MemoryUsage;
-import java.lang.reflect.Parameter;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,9 +26,14 @@ public class UserController {
         return "로그인 페이지";
     }
 
-    @GetMapping("{user_id}")
-    public UserResponseDto user_profile(@PathVariable("user_id") Long user_id) {
-        return userService.profile(user_id);
+    @GetMapping("/user/{user_id}")
+    public ResponseEntity<FileSystemResource> user_profile(@PathVariable("user_id") Long user_id) throws IOException {
+        return userService.getProfile_img(user_id);
+    }
+
+    @GetMapping("user_name/{user_id}")
+    public String user_profile_name(@PathVariable("user_id") Long user_id) {
+        return userService.getProfile_name(user_id);
     }
 
     @PostMapping("/user/register")
@@ -43,11 +46,15 @@ public class UserController {
         return userService.login(userLoginDto);
     }
 
-    @PutMapping("{user_id}/update")
+    @PatchMapping("{user_id}/update")
     public void UpdateProfile(
             @PathVariable("user_id") Long user_id,
-            @RequestBody UserUpdateDto userUpdateDto,
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "password") String password,
+            @RequestParam(value = "newPassword") String newPassword,
             @RequestParam MultipartFile file) throws IOException {
-        userService.updateProfile(user_id, userUpdateDto, file);
+        userService.updateProfile(user_id, name, password, newPassword, file);
+//        userService.updateProfile(user_id, file);
+
     }
 }
