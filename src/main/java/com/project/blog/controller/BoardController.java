f1336b89e81086b2050f9c6ff3ebf7a50e8eec35
SungGil-5125 <sunggil0125@naver.com>
2022-05-30 20:46:01 +0900

    ✨ :: 블로그 생성

--- src/main/java/com/project/blog/controller/BoardController.java
+++ src/main/java/com/project/blog/controller/BoardController.java
@@ -1,28 +1,34 @@
 package com.project.blog.controller;
 
 import com.project.blog.domain.Board;
+import com.project.blog.domain.User;
 import com.project.blog.dto.Request.BoardCreateDto;
 import com.project.blog.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
-    @PostMapping("board/write")
+    @PostMapping("{user_email}/board/write")
     public Board CreateBoard(
-            @Valid @RequestParam MultipartFile file,
-            @Valid @RequestPart BoardCreateDto boardCreateDto
-            ) throws Exception {
-        return boardService.CreateBoard(file, boardCreateDto);
+            @PathVariable("user_email") String user_email,
+            @RequestParam MultipartFile file,
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "content") String content,
+            @RequestParam(value = "date") String date
+    ) throws Exception {
+
+        return boardService.CreateBoard(user_email, file, title, content, date);
+
     }
 
     // 이미지 여러개는 for문 조지면 될려나
--- src/main/java/com/project/blog/service/BoardService.java
+++ src/main/java/com/project/blog/service/BoardService.java
@@ -1,76 +1,86 @@
 package com.project.blog.service;
 
 import com.project.blog.domain.Board;
+import com.project.blog.domain.Image;
 import com.project.blog.domain.User;
 import com.project.blog.dto.Request.BoardCreateDto;
 import com.project.blog.exception.CustomException;
 import com.project.blog.repository.BoardRepository;
 import com.project.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageReader;
 import java.io.File;
 import java.io.IOException;
 
 import static com.project.blog.exception.ErrorCode.IMAGE_NOT_FOUND;
 import static com.project.blog.exception.ErrorCode.USER_NOT_FIND;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
+//    private final
 
     @Transactional
-    public Board CreateBoard(MultipartFile file, BoardCreateDto boardCreateDto) throws Exception {
+    public Board CreateBoard(String user_email, MultipartFile file, String title, String content, String date) throws IOException {
 
-        Board board = boardCreateDto.toEntity();
-        board.userMapping();
+        User user = userRepository.findByEmail(user_email)
+                .orElseThrow(()-> new CustomException(USER_NOT_FIND));
 
-        updateBoard_image(file);
+        BoardCreateDto boardCreateDto = BoardCreateDto.builder()
+                .title(title)
+                .content(content)
+                .date(date)
+                .build();
+
+        Board board = boardCreateDto.toEntity(user);
+
+        updateBoard_image(user_email, file, board);
 
         return boardRepository.save(board);
-
-        //jwt를 쓰면 header에 저장되는데 이게 postman header?
-        //
-
     }
 
     @Transactional
-    public void updateBoard_image(MultipartFile file) throws IOException {
+    public void updateBoard_image(String user_email, MultipartFile file, Board board) throws IOException {
 
         if(file.isEmpty()) {
             throw new CustomException(IMAGE_NOT_FOUND);
         }
 
-        //사진 업로드
         String absolutePath = new File("").getAbsolutePath() + "\\";
-        String path = "profile" + File.separator; //current_date
+        String path = "board_image" +File.separator + user_email + File.separator; //current_date
         File folder = new File(path);
 
-        if(!folder.exists()) {
+        if (!folder.exists()) {
             folder.mkdirs();
         }
 
         String originalFileExtension = null;
         String contentType = file.getContentType();
 
-        if(contentType.contains("image/jpeg") || contentType.contains("image/png")) {
+        if (contentType.contains("image/jpeg") || contentType.contains("image/png")) {
             originalFileExtension = ".jpg";
         }
 
-        //String new_file_name = user_id + originalFileExtension;
+        String file_name = file + originalFileExtension;
 
-        //folder = new File(absolutePath + path + File.separator + new_file_name);
+        folder = new File(absolutePath + path + File.separator + file_name);
+
+        Image image = Image.builder()
+                .board(board)
+                .url(String.valueOf(folder))
+                .build();
+
+
+
         file.transferTo(folder);
-
-
     }
-
-
-
-
 }
