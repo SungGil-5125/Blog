2650bbfe34816be056772c1f1d87144dfa512c65
SungGil-5125 <sunggil0125@naver.com>
2022-06-02 19:33:52 +0900

    ✨ :: toEntity user 추가

--- src/main/java/com/project/blog/dto/Request/BoardCreateDto.java
+++ src/main/java/com/project/blog/dto/Request/BoardCreateDto.java
@@ -3,27 +3,28 @@ package com.project.blog.dto.Request;
 import com.project.blog.domain.Board;
 import com.project.blog.domain.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class BoardCreateDto {
 
     private Long board_id;
-    private User user_id;
     private String title;
-    private String board_image;
     private String content;
     private String date;
 
-    public Board toEntity() {
+    public Board toEntity(User user) {
         return Board.builder()
-                .user(user_id)
                 .title(title)
                 .content(content)
                 .date(date)
+                .user(user)
                 .build();
     }
+
 }
