b95d5bf1e280407fe78e470efc0c9f3211cd8520
SungGil-5125 <sunggil0125@naver.com>
2022-06-02 23:27:49 +0900

    ✨ :: 삭제

--- src/main/java/com/project/blog/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.blog.repository;
-
-import com.project.blog.domain.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Long, Image> {
-}
