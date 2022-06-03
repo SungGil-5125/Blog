140990d23c35236f545d24f978f0b832639b0756
SungGil-5125 <sunggil0125@naver.com>
2022-06-03 15:16:12 +0900

    ✨ :: BoardResponse

---
+++ src/main/java/com/project/blog/dto/Response/BoardResponseDto.java
@@ -0,0 +1,19 @@
+package com.project.blog.dto.Response;
+
+import com.project.blog.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class BoardResponseDto {
+
+    private Long board_id;
+    private String title;
+    private String content;
+    private String date;
+    private User user;
+
+}
