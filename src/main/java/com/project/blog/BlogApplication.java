5b76aa8beb61c58c302b529eddaf49312ee66015
SungGil-5125 <sunggil0125@naver.com>
2022-06-11 22:02:20 +0900

    🎉 :: aws.yml 설정

--- src/main/java/com/project/blog/BlogApplication.java
+++ src/main/java/com/project/blog/BlogApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml"
+			+ "classpath:aws.yml"; //application.yml 과 aws.yml 두개의 파일 모두를 설정 파일로 읽어서 사용한다.
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
