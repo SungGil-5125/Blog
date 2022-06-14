16eeca671e880f15cdc1370b9871952161531a15
SungGil-5125 <sunggil0125@naver.com>
2022-06-14 21:42:32 +0900

    ✨ :: validation 추가

--- src/main/java/com/project/blog/dto/Request/UserSignupDto.java
+++ src/main/java/com/project/blog/dto/Request/UserSignupDto.java
@@ -9,18 +9,20 @@ import lombok.*;
 
 @Getter
 @Setter
-@AllArgsConstructor //전체 인자를 갖는 생성자를 만들어줌
+@NoArgsConstructor
+//@AllArgsConstructor //전체 인자를 갖는 생성자를 만들어줌
 public class UserSignupDto {
 
-    @NotBlank(message = "이메일이 입력되지 않았습니다.")
-    @Email(message = "이메일 형식에 맞지 않습니다.")
+    @NotBlank
+    //@Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
+    @Email
     private String email;
 
     @NotBlank(message = "사용자 이름이 입력되지 않았습니다.")
     private String name;
 
     @NotBlank(message = "패스워드가 입력되지 않았습니다.")
-    @Size(min = 8, message = "비밀번호는 8자 이상이여야 합니다.")
+    @Size(min = 4, message = "비밀번호는 4자 이상이여야 합니다.")
     private String password;
 
     public User toEntity(){
