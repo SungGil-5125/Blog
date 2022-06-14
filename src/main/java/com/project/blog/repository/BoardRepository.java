66a06264d36a1545b78a556ab9d9dcbafbf6f955
SungGil-5125 <sunggil0125@naver.com>
2022-06-14 21:43:57 +0900

    ✨ :: user_id로 블로그 찾는 메서드 추가

--- src/main/java/com/project/blog/repository/BoardRepository.java
+++ src/main/java/com/project/blog/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.project.blog.repository;
 
 import com.project.blog.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
    List<Board> findAll();
 
+   @Query(nativeQuery = true, value = "select b.board_id, b.content, b.title, b.date, b.url, b.user_id from Board b where b.user_id=:user_id")
+   List<Board> findByUser_Id(Long user_id);
 }
