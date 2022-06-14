f397b01ff38d919026232187ee1de19157ab780f
SungGil-5125 <sunggil0125@naver.com>
2022-06-14 21:42:52 +0900

    ✨ :: CustomException

--- src/main/java/com/project/blog/exception/CustomException.java
+++ src/main/java/com/project/blog/exception/CustomException.java
@@ -3,21 +3,10 @@ package com.project.blog.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor //모든 필드의 값을 가지는 생성자를 만들어주자나 그러면
+@AllArgsConstructor
 @Getter
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
-
-
-    /*
-
-    private Errorcode(errorcode) {
-
-        this.errorcode = errorcode; 이런 느낌?
-
-    }
-
-     */
 
 }
