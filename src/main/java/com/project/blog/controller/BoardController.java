a0376627260aa5fbc2ee20075d2797d600e21f1b
SungGil-5125 <sunggil0125@naver.com>
2022-06-15 20:59:46 +0900

    ✨ :: parameter naming

--- src/main/java/com/project/blog/controller/BoardController.java
+++ src/main/java/com/project/blog/controller/BoardController.java
@@ -23,10 +23,10 @@ public class BoardController {
     private final BoardService boardService;
     private final ResponseService responseService;
 
+    // 게시글 생성
     @PostMapping("board/write")
     public CommonResultResponse CreateBoard(
             @RequestPart(value = "file", required = false) MultipartFile multipartFile,
-//            @RequestPart(value = "boardCreateDto") BoardCreateDto boardCreateDto
             @RequestParam(value = "title") String title,
             @RequestParam(value = "content") String content,
             @RequestParam(value = "date") String date
@@ -44,32 +44,33 @@ public class BoardController {
     }
 
     // 게시글 삭제
-    @DeleteMapping("/Delete/board/{board_id}")
+    @DeleteMapping("/delete/{board_id}")
     public CommonResultResponse DeleteBlog(@PathVariable("board_id") Long board_id) {
         boardService.DeleteBlog(board_id);
         return responseService.getSuccessResult();
     }
 
-    // 전체 게시글 불러오기
+    // 전체 게시글 불러 오기
     @GetMapping("board")
     public AllBoardListResponseDto showAllBlog() {
         return boardService.getAllBoards();
     }
 
+    // 게시글 이미지 보여 주기
     @GetMapping("board_image/{board_id}")
-    public String showAllBlogImage(@PathVariable("board_id") Long board_id) throws IOException {
+    public String showAllBlogImage(@PathVariable("board_id") Long board_id) {
         return boardService.getAllBoardsImage(board_id);
     }
 
+    // 나의 게시글들 보기 <- 이거는 보류...
     @GetMapping("board/myBoard")
     public BoardListResponseDto getMyBoards() {
        return boardService.getMyBoards();
     }
 
-    @GetMapping("/otherBoard/{user_id}")
+    // 다른 사람 게시글들 보기
+    @GetMapping("/boards/{user_id}")
     public BoardListResponseDto getOtherBoards(@PathVariable("user_id") Long user_id) {
         return boardService.getOthersBoards(user_id);
     }
-
-
 }
